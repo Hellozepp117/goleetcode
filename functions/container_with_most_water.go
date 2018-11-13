af43f383d70b036e372ad6f8f7410a1a5367e9bd
bolun_xu <bolun_xu@arcplus.com.cn>
2018-11-13 14:32:55 +0800

    feat:container_with_most_water

--- functions/container_with_most_water.go
+++ functions/container_with_most_water.go
@@ -1,5 +1,23 @@
 package functions
 
 func maxArea(height []int) int {
-	return 0
+	n := len(height)
+	var i, j, area int
+	for j = n - 1; i < j; {
+		var a int
+		if height[i] > height[j] {
+			a = (height[i] - height[j]) * (j - i)
+		} else {
+			a = (height[j] - height[i]) * (j - i)
+		}
+		if area < a {
+			area = a
+		}
+		if height[i+1] > height[j-1] {
+			i++
+		} else {
+			j--
+		}
+	}
+	return area
 }
