4e1a4d41f8d76590bb4e04d048b31bd1c0102508
bolun_xu <bolun_xu@arcplus.com.cn>
2018-11-13 18:07:12 +0800

    fix:inttoroman

--- functions/integer_to_roman.go
+++ functions/integer_to_roman.go
@@ -1,7 +1,15 @@
 package functions
 
 func intToRoman(num int) string {
-    
+	var ans string
+	for i:=1;i<=num/1000;i++{
+		ans=ans+"M"
+	}
+	num=num%1000
+	if num/900==1{
+		ans =ans+"CM"
+	}
+	return
 }
 M1000
 D500
