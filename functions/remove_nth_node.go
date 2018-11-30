79505413a513fa58545f32005a65e40984b6b7e4
bolun_xu <bolun_xu@arcplus.com.cn>
2018-11-30 15:18:54 +0800

    feat:removenthnoide

--- functions/remove_nth_node.go
+++ functions/remove_nth_node.go
@@ -2,9 +2,14 @@ package functions
 
 /**
  * Definition for singly-linked list.
- **/
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	base := head
 
 	return nil
 }
