5cb302edb00fb9d6fc7d163428f560508cef5682
bolun_xu <bolun_xu@arcplus.com.cn>
2018-11-30 10:01:39 +0800

    feat:3sumclosest

--- functions/three_sum_closest.go
+++ functions/three_sum_closest.go
@@ -6,32 +6,40 @@ import (
 
 func threeSumClosest(nums []int, target int) int {
 	sort.Ints(nums)
-	var ans [][]int
+	closestSum := nums[0] + nums[1] + nums[2]
 	for i := 0; i+2 < len(nums); i++ {
 		if i > 0 && nums[i] == nums[i-1] { // skip same result
 			continue
 		}
 		j := i + 1
 		k := len(nums) - 1
-		tar := -nums[i] + target
 		for j < k {
-			if nums[j]+nums[k] > target {
-				k--
-			} else if nums[j]+nums[k] == target {
-				ans = append(ans, []int{nums[i], nums[j], nums[k]})
-				j++
-				k--
-				for j < k && nums[j] == nums[j-1] {
-					j++
-				}
-				for j < k && nums[k] == nums[k+1] {
+			threeSum := nums[i] + nums[j] + nums[k]
+			if toAbs(threeSum-target) < toAbs(closestSum-target) {
+				closestSum = threeSum
+			}
+			if threeSum == target {
+				return target
+			} else if threeSum > target {
+				for j < k && nums[k] == nums[k-1] {
 					k--
 				}
+				k--
 			} else {
+				for j < k && nums[j] == nums[j+1] {
+					j++
+				}
 				j++
 			}
 		}
 
 	}
-	return ans
+	return closestSum
+}
+
+func toAbs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
