026f2626d3b1a1c487583dbd2856c7e9fdfe63bf
bolun_xu <bolun_xu@arcplus.com.cn>
2018-12-28 15:39:06 +0800

    feat:valid_parentheses

--- functions/valid_parentheses.go
+++ functions/valid_parentheses.go
@@ -1,6 +1,36 @@
 package functions
 
-func isValid(s string) bool {
+import "container/list"
 
+func isValid(s string) bool {
+	target := []rune(s)
+	l := list.New()
+	for _, v := range target {
+		switch v {
+		case '(', '[', '{':
+			l.PushBack(v)
+		case ')':
+			if l.Back() != nil && l.Back().Value == '(' {
+				l.Remove(l.Back())
+			} else {
+				return false
+			}
+		case ']':
+			if l.Back() != nil && l.Back().Value == '[' {
+				l.Remove(l.Back())
+			} else {
+				return false
+			}
+		case '}':
+			if l.Back() != nil && l.Back().Value == '{' {
+				l.Remove(l.Back())
+			} else {
+				return false
+			}
+		}
+	}
+	if l.Len() == 0 {
+		return true
+	}
 	return false
 }
