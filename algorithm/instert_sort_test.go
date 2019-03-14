14b1e15b26189a9812a031aa6d03dad967bd2afa
Bolun Xu <bolun_xu@arcplus.com.cn>
2019-03-14 12:12:43 +0800

    feat: bubble sort

--- algorithm/instert_sort_test.go
+++ algorithm/instert_sort_test.go
@@ -6,7 +6,13 @@ import (
 )
 
 func TestInsertSort(t *testing.T) {
-	list := []float64{}
+	list := []float64{2, 4, 3, 6, 5, 1}
 	result := InsertSort(list)
+	fmt.Println(result)
+}
+
+func TestBubbleSort(t *testing.T) {
+	list := []float64{2, 1}
+	result := BubbleSort(list)
 	fmt.Println(result)
 }
